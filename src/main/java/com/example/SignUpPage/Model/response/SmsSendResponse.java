233a336d29b66d03008b845fb0dcdcd455227e89
BuckyHocadev <144876206+BuckyHocadev@users.noreply.github.com>
2023-09-15 10:05:59 +0500

    Update SmsSendResponse.java

--- src/main/java/com/example/SignUpPage/Model/response/SmsSendResponse.java
+++ src/main/java/com/example/SignUpPage/Model/response/SmsSendResponse.java
@@ -3,17 +3,10 @@ package com.example.SignUpPage.Model.response;
 public class SmsSendResponse {
 
     private String time;
-    /**
-     * 消息id
-     */
+
     private String msgId;
-    /**
-     * 状态码说明（成功返回空）
-     */
+
     private String errorMsg;
-    /**
-     * 状态码（详细参考提交响应状态码）
-     */
     private String code;
     public String getTime() {
         return time;
